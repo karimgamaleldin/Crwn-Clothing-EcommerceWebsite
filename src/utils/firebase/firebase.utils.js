efd15f0d47952cecc479719bd615ee992a2ed025
Karim Gamaleldin <karimgamaleldin74@gmail.com>
2022-12-25 09:20:35 +0200

    sign in with google done

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,6 +3,12 @@ import {getAuth,
         signInWithRedirect,
         signInWithPopup,
         GoogleAuthProvider} from 'firebase/auth'; 
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCr-CwMTs5v0E9wCqf6QAng88xJ7lTEHv4",
@@ -17,9 +23,33 @@ const firebaseConfig = {
   const firebaseApp = initializeApp(firebaseConfig);
 
   const provider = new GoogleAuthProvider();
-  provider.setCustomParameters({
-    prompt: "select-account"
-  });
+  const x = {
+    prompt: 'select_account',
+  }
+  provider.setCustomParameters(x);
 
-  export const auth = getAuth();
- export const signInWithGooglePopup = () => signInWithPopup(auth , provider); 
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth , provider); 
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db , 'users' , userAuth.uid);
+  console.log(userDocRef);
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  if (!userSnapshot.exists()) {
+    const {displayName , email} = userAuth;
+    const createdAt = new Date(); 
+    try {
+      await setDoc(userDocRef,{displayName,email,createdAt});
+    }catch (error){
+      console.log(error.message);
+    }
+  }
+  //if user data doesnot exists 
+  //create /set the document with the data from userAuth in my collection
+
+  //if user data exists
+  return userDocRef;
+ };;
