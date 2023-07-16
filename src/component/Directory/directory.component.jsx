037723fd9da7cfe7fba4d787fe550f47026bc145
Karim Gamaleldin <karimgamaleldin74@gmail.com>
2023-07-16 23:23:21 +0300

    Categories fix

--- src/component/Directory/directory.component.jsx
+++ src/component/Directory/directory.component.jsx
@@ -4,32 +4,32 @@ import './directory.styles.scss';
 const categories = [
     {
       "id": 1,
-      "title": "hats",
+      "title": "Hats",
       "imageUrl": "https://i.ibb.co/cvpntL1/hats.png",
       route : 'shop/hats'
        
     },
     {
       "id": 2,
-      "title": "jackets",
+      "title": "Jackets",
       "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png",
       route : 'shop/jackets'
     },
     {
       "id": 3,
-      "title": "sneakers",
+      "title": "Sneakers",
       "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png",
       route : 'shop/sneakers'
     },
     {
       "id": 4,
-      "title": "womens",
+      "title": "Women",
       "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png",
       route : 'shop/womens'
     },
     {
       "id": 5,
-      "title": "mens",
+      "title": "Men",
       "imageUrl": "https://i.ibb.co/R70vBrQ/men.png",
       route : 'shop/mens'
     }
